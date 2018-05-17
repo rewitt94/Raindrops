537b4796ac5172beded892a487c377771815ee94
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-05-17 11:34:59 +0100

    different test cases added

--- test/test.js
+++ test/test.js
@@ -18,10 +18,20 @@ describe('#ModulusMap', function() {
       assert.equal(number(1003), 1003);
     });
 
-    it('returns object value if input divides an object key', function() {
-      mapper = ModulusMap({2: 'hello'})
-      assert.equal(mapper(10), 'hello');
+    it('returns object value if object key is a factor of input', function() {
+      mapper = ModulusMap({2: 'return me!'})
+      assert.equal(mapper(10), 'return me!');
     });
+
+    it('retuns input if object key is not a factor of input', function() {
+      mapper = ModulusMap({3: 'do not return!'})
+      assert.equal(mapper(10), 10);
+    });
+
+    it('returns corresponding object value if an object key is a factor of input', function() {
+      mapper = ModulusMap({7: 'pick me!', 5: 'do not pick me!'})
+      assert.equal(mapper(21), 'pick me!');
+    })
 
   });
 
